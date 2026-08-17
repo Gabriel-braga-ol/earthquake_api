45292a9bb923ed84029f790a59b9e7217d41cec7
Gabriel Braga <bielbraga37@outlook.com>
2026-08-16 22:06:21 -0300

    feat: cria model Earthquake e migration inicial

--- earthquakes/models.py
+++ earthquakes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Earthquake(models.Model):
+    external_id = models.CharField(max_length=50, unique=True)
+    magnitude = models.FloatField()
+    place = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    depth = models.FloatField()
+    time = models.DateTimeField()
+    sig = models.IntegerField()
+    fetched_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.place} {self.magnitude}'
