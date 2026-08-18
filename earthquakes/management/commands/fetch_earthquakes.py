4f172b960061f60e0209a37aebeffc089a03b5a9
Gabriel Braga <bielbraga37@outlook.com>
2026-08-18 17:44:14 -0300

    feat: implementa loop de extração e persistência dos terremotos via update_or_create

--- earthquakes/management/commands/fetch_earthquakes.py
+++ earthquakes/management/commands/fetch_earthquakes.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from earthquakes.models import Earthquake
 import requests 
 
 class Command(BaseCommand):
@@ -19,8 +20,33 @@ class Command(BaseCommand):
         if response.status_code == 200:
             data = response.json()
             self.stdout.write(f"Terremotos encontrados: {len(data['features'])}")
+
+            for feature in data["features"]:
+                magnitude = feature["properties"]["mag"]
+                place = feature["properties"]["place"]
+                timestamp_ms = feature["properties"]["time"]
+                sig = feature["properties"]["sig"]
+                external_id = feature["id"]
+                longitude = feature["geometry"]["coordinates"][0]
+                latitude = feature["geometry"]["coordinates"][1]
+                depth = feature["geometry"]["coordinates"][2]  
+    
+                event_time = datetime.fromtimestamp(timestamp_ms / 1000, tz=timezone.utc)
+    
+                Earthquake.objects.update_or_create (
+                    external_id = external_id,
+                    defaults= {
+                        "magnitude": magnitude,
+                        "place": place,
+                        "latitude": latitude,
+                        "longitude": longitude,
+                        "depth": depth,
+                        "time": event_time,
+                        "sig": sig,
+                    }
+                )
         else:
-            raise CommandError(f"Erro ao buscar dados: {response.status_code}")
+            raise CommandError(f"Erro ao buscar dados: {response.status_code}")           
 
 
 
