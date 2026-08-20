c659b87413c5bc6008e7c14c48d57871fd23c054
Gabriel Braga <bielbraga37@outlook.com>
2026-08-19 23:37:51 -0300

    feat: adiciona filtro de magnitude no frontend com atualização dinâmica do mapa

--- earthquake_map/script.js
+++ earthquake_map/script.js
@@ -1,3 +1,5 @@
+document.getElementById('botao-filtrar').addEventListener('click', buscarTerremotos);
+
 // cria e posiciona o mapa
 const map = L.map('mapa').setView([0, 20], 2); 
 
@@ -6,8 +8,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+const grupoDeMarcadores = L.layerGroup().addTo(map);
+
 async function buscarTerremotos() {
-    const resposta = await fetch('http://127.0.0.1:8000/api/earthquakes/?page_size=1000');
+    grupoDeMarcadores.clearLayers();
+
+    const magnitudeMinima = document.getElementById('magnitude-minima').value;
+
+    let url = 'http://127.0.0.1:8000/api/earthquakes/?page_size=1000';
+    if (magnitudeMinima) {
+        url += `&min_magnitude=${magnitudeMinima}`;
+    }
+
+    const resposta = await fetch(url);
     const dados = await resposta.json();
     console.log(dados);
 
@@ -15,8 +28,7 @@ async function buscarTerremotos() {
         const dataFormatada = new Date(terremoto.time).toLocaleString('pt-BR');
 
         L.marker([terremoto.latitude, terremoto.longitude])
-        .bindPopup(`<b>${terremoto.place}</b><br>Magnitude: ${terremoto.magnitude}<br>Profundidade: ${terremoto.depth} km<br>Data: ${dataFormatada}`)
-        .addTo(map);
+        .bindPopup(`<b>${terremoto.place}</b><br>Magnitude: ${terremoto.magnitude}<br>Profundidade: ${terremoto.depth} km<br>Data: ${dataFormatada}`).addTo(grupoDeMarcadores)
     }
 }
 
