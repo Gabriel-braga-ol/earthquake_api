9133317c2fc7c5254951944f9f2c75b3d30dd8f4
Gabriel Braga <bielbraga37@outlook.com>
2026-08-28 22:02:19 -0300

    feat: adiciona as coordendas dos terremotos no globo

--- earthquake_map/script.js
+++ earthquake_map/script.js
@@ -1,3 +1,17 @@
 const globo = Globe()
     .globeImageUrl('https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
     (document.getElementById('globo'));
+
+async function buscarTerremotos() {
+    const resposta = await fetch('http://127.0.0.1:8000/api/earthquakes/?page_size=1000');
+    const dados = await resposta.json();
+
+    globo
+        .pointsData(dados.results)
+        .pointLat('latitude')
+        .pointLng('longitude')
+        .pointColor(() => 'red')
+        .pointAltitude(0.01);
+}
+
+buscarTerremotos();
